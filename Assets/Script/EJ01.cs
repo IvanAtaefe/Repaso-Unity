035deb05a45c8cf94140b6a20acfebeeaa0d9afd
IvanAtaefe <88833116+IvanAtaefe@users.noreply.github.com>
2022-04-08 11:32:25 -0300

    EJ01 y EJ02

---
+++ Assets/Script/EJ01.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//1.Crea un programa que permita ingresar por Inspector dos valores enteros y responda si son iguales o no lo son.
+
+public class EJ01 : MonoBehaviour
+{
+
+    public int valor1;
+    public int valor2;
+    void Start()
+    {
+        if (valor1 == valor2) {
+            Debug.Log("Son iguales");
+        }
+        else {
+            Debug.Log("No son iguales");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/EJ02.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EJ02 : MonoBehaviour
+{
+    //2. Crear las variables necesarias para ingresar por Inspector el precio de 3 productos y un monto de dinero disponible.Comunicar si la suma de los mismos supera o no el monto de dinero disponible.Luego indicar cuánto dinero sobra o falta.
+    public int producto1;
+    public int producto2;
+    public int producto3;
+    public int montodedinero;
+    int total;
+    void Start()
+    {
+        total = producto1 + producto2 + producto3;
+        if (montodedinero < total)
+        {
+            Debug.Log("La suma de los productos supera el monto. Te faltan $" + (total - montodedinero) + ".");
+        }
+        else
+        {
+            Debug.Log("La suma de los productos no supera el monto. Te sobran $" + (montodedinero - total) + ".");
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
